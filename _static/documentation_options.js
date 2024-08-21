8a6a042b6a88f4e507aa857ead5b773fba9ad10f
rcjackson <rcjackson@users.noreply.github.com>
2024-08-21 16:31:07 +0000

    deploy: 026da5d865f0e7bd7b0c9ea02f4fd62ee9c0077f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.0.post1064',
+    VERSION: '1.8.0.post1111',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
